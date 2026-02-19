53b36f6cffb2e8aa610fd4eccd23ac2a0c9ef771
zahirguzel <zahirrguzel@gmail.com>
2026-02-19 12:26:32 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+
+            var degerler = db.TblHakkimda.ToList();
+
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+
+            var deneyimler = db.TblDeneyimlerim.ToList();
+
+            return PartialView(deneyimler); // birden fazla tablo döndürmek için partialview kullandık (view tarafında birden fazla  bölme işlemi yapılacksa kullanılır)
+        }
+
+        public PartialViewResult Egitim()
+        {
+
+            var egitim = db.TblEgitimlerim.ToList();
+
+            return PartialView(egitim); 
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+
+            var yetenekler = db.TblYeteneklerim.ToList();
+
+            return PartialView(yetenekler);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+
+            var hobiler = db.TblHobilerim.ToList();
+
+            return PartialView(hobiler);
+        }
+
+        public PartialViewResult Sertifikalar()
+        {
+
+            var sertifika = db.TblSertifikalarım.ToList();
+
+            return PartialView(sertifika);
+        }
+
+        [HttpGet] // verileri .çeken 
+        public PartialViewResult iletisim()
+        {
+
+
+            return PartialView();
+        }
+
+        [HttpPost] // butona basıldığında verileri yollayan
+        public PartialViewResult iletisim(Tbliletisim t)
+        {
+
+            t.Tarih=  DateTime.Parse( DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(t);
+            db.SaveChanges();
+            
+
+
+            return PartialView();
+        }
+    }
+}
